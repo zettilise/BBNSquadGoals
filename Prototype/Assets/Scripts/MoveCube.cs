a70628b7e05f22779334873ebd7d09dacc321f45
Unknown <zettilise.cho@gmail.com>
2018-04-24 19:43:27 -0400

    General layout for prototype, no additional mechanics

---
+++ Prototype/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z+=.1f;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.z-=.1f;
+			this.transform.position = position;
+		}
+	}
+}

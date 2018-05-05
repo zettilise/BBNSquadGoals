5a01a41a719147bf50d7b41b9a950e2e33a4fad1
AliaRizvi <you@example.com>
2018-05-05 13:05:00 -0400

    Almost pooled

---
+++ Prototype/Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+	// Use this for initialization
+
+	public string tag;
+
+	public Transform position;
+
+	public Quaternion rotation;
+
+
+	private ObjectPooler objectPooler;
+
+
+
+	void Start(){
+		objectPooler = ObjectPooler.Instance;
+	}
+
+
+	void FixedUpdate(){
+		ObjectPooler.Instance.SpawnFromPool ("Tomato", transform.position, Quaternion.identity);
+
+	}
+}
